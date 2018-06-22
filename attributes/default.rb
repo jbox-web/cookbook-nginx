022bba418bca06a950904817a891223a7f358de9
Nicolas Rodriguez <nicoladmin@free.fr>
2018-06-22 08:10:42 +0200

    Coding style, add comments, add test

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,28 +1,29 @@
-default['nginx']['server']['config']['worker_processes']                      = node['cpu'] && node['cpu']['total'] ? node['cpu']['total'] : 1
-default['nginx']['server']['config']['include']                               = '/etc/nginx/modules-enabled/*.conf'
-default['nginx']['server']['config']['pid']                                   = '/run/nginx.pid'
-default['nginx']['server']['config']['user']                                  = 'www-data'
-default['nginx']['server']['config']['events']                                = {}
-default['nginx']['server']['config']['events']['worker_connections']          = 1024
-default['nginx']['server']['config']['http']                                  = {}
-default['nginx']['server']['config']['http']['access_log']                    = 'off'
-default['nginx']['server']['config']['http']['error_log']                     = '/var/log/nginx/error.log'
-default['nginx']['server']['config']['http']['server_tokens']                 = 'off'
-default['nginx']['server']['config']['http']['default_type']                  = 'application/octet-stream'
-default['nginx']['server']['config']['http']['sendfile']                      = 'on'
-default['nginx']['server']['config']['http']['tcp_nopush']                    = 'on'
-default['nginx']['server']['config']['http']['tcp_nodelay']                   = 'on'
-default['nginx']['server']['config']['http']['keepalive_timeout']             = 10
-default['nginx']['server']['config']['http']['client_max_body_size']          = '1m'
-default['nginx']['server']['config']['http']['gzip']                          = 'on'
-default['nginx']['server']['config']['http']['gzip_disable']                  = 'msie6'
-default['nginx']['server']['config']['http']['gzip_vary']                     = 'on'
-default['nginx']['server']['config']['http']['gzip_proxied']                  = 'any'
-default['nginx']['server']['config']['http']['gzip_comp_level']               = 6
-default['nginx']['server']['config']['http']['gzip_http_version']             = 1.1
-default['nginx']['server']['config']['http']['gzip_min_length']               = 1000
-default['nginx']['server']['config']['http']['gzip_buffers']                  = '16 8k'
-default['nginx']['server']['config']['http']['gzip_types']                    = %w[
+# Default Nginx config
+default['nginx']['server']['config']['worker_processes']             = node['cpu'] && node['cpu']['total'] ? node['cpu']['total'] : 1
+default['nginx']['server']['config']['include']                      = '/etc/nginx/modules-enabled/*.conf'
+default['nginx']['server']['config']['pid']                          = '/run/nginx.pid'
+default['nginx']['server']['config']['user']                         = 'www-data'
+default['nginx']['server']['config']['events']                       = {}
+default['nginx']['server']['config']['events']['worker_connections'] = 1024
+default['nginx']['server']['config']['http']                         = {}
+default['nginx']['server']['config']['http']['access_log']           = 'off'
+default['nginx']['server']['config']['http']['error_log']            = '/var/log/nginx/error.log'
+default['nginx']['server']['config']['http']['server_tokens']        = 'off'
+default['nginx']['server']['config']['http']['default_type']         = 'application/octet-stream'
+default['nginx']['server']['config']['http']['sendfile']             = 'on'
+default['nginx']['server']['config']['http']['tcp_nopush']           = 'on'
+default['nginx']['server']['config']['http']['tcp_nodelay']          = 'on'
+default['nginx']['server']['config']['http']['keepalive_timeout']    = 10
+default['nginx']['server']['config']['http']['client_max_body_size'] = '1m'
+default['nginx']['server']['config']['http']['gzip']                 = 'on'
+default['nginx']['server']['config']['http']['gzip_disable']         = 'msie6'
+default['nginx']['server']['config']['http']['gzip_vary']            = 'on'
+default['nginx']['server']['config']['http']['gzip_proxied']         = 'any'
+default['nginx']['server']['config']['http']['gzip_comp_level']      = 6
+default['nginx']['server']['config']['http']['gzip_http_version']    = 1.1
+default['nginx']['server']['config']['http']['gzip_min_length']      = 1000
+default['nginx']['server']['config']['http']['gzip_buffers']         = '16 8k'
+default['nginx']['server']['config']['http']['gzip_types']           = %w[
   text/plain
   text/css
   text/xml
@@ -38,7 +39,7 @@ default['nginx']['server']['config']['http']['gzip_types']                    = 
   application/xhtml+xml
   image/svg+xml
 ].join(' ')
-default['nginx']['server']['config']['http']['include']                       = %w[
+default['nginx']['server']['config']['http']['include']              = %w[
   /etc/nginx/mime.types
   /etc/nginx/conf.d/*.conf
   /etc/nginx/sites-enabled/*
--- test/integration/default/controls/default.rb
+++ test/integration/default/controls/default.rb
@@ -2,14 +2,26 @@
 
 title 'Test Nginx installation'
 
+# Test Nginx package
 describe package('nginx') do
   it { should be_installed }
 end
 
+# Test Nginx config
 describe file('/etc/nginx/nginx.conf') do
   it { should exist }
 end
 
+output = """
+nginx: the configuration file /etc/nginx/nginx.conf syntax is ok
+nginx: configuration file /etc/nginx/nginx.conf test is successful
+"""
+
+describe command('nginx -t -c /etc/nginx/nginx.conf') do
+  its('stderr') { should eq output.lstrip }
+end
+
+# Test Nginx service
 describe service('nginx') do
   it { should be_installed }
   it { should be_enabled }
