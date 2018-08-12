310727d0783571697fd5d1bdea8c102177f4c1fa
Nicolas Rodriguez <nicoladmin@free.fr>
2018-08-12 07:17:02 +0200

    Configure logrotate

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,6 +18,16 @@ template '/etc/nginx/nginx.conf' do
   variables config: node[:nginx][:server][:config]
 end
 
+# Configure logrotate
+cookbook_file '/etc/logrotate.d/nginx' do
+  source 'logrotate/nginx'
+end
+
+# Create Nginx log archive dir
+directory '/var/log/OLD_LOGS/nginx' do
+  recursive true
+end
+
 # Restart Nginx when config is changed
 service 'nginx' do
   subscribes :restart, 'template[/etc/nginx/nginx.conf]', :immediately
