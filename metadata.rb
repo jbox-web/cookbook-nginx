3e64e0c0a60ffd5a74731a0614ff6f658a7553fc
Nicolas Rodriguez <nico@nicoladmin.fr>
2021-03-19 15:49:53 +0100

    Change cookbook name to not clash with existing ones

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-name             'nginx'
+name             'jbox-nginx'
 maintainer       'Nicolas Rodriguez'
-maintainer_email 'nicoladmin@free.fr'
+maintainer_email 'nico@nicoladmin.fr'
 license          'MIT'
 description      'Installs/Configures Nginx'
 version          '1.0.0'
